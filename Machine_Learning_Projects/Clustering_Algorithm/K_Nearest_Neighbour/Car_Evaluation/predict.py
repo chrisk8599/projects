d1245991a29a07916c4bd36961d57d2ec465bf47
Chris Kim <ckim5291@uni.sydney.edu.au>
2021-01-19 15:47:05 +1100

    Fixed

--- Machine_Learning_Projects/Clustering_Algorithm/K_Nearest_Neighbour/Car_Evaluation/predict.py
+++ Machine_Learning_Projects/Clustering_Algorithm/K_Nearest_Neighbour/Car_Evaluation/predict.py
@@ -1,9 +1,7 @@
 import sklearn
-from sklearn.utils import shuffle
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
-import numpy as np
-from sklearn import linear_model, preprocessing
+from sklearn import preprocessing
 
 data = pd.read_csv("car.data")
 
